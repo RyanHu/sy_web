d039e9220bb7ed6aeae34791074d99ec8e1a5d7c
Ryan <huboyang525@gmail.com>
2020-08-19 18:44:53 +0800

    使用饿了么ui
    init

---
+++ src/components/Login.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-container style="width: 20%; margin-left: 40%" >
+    <el-header>登录系统</el-header>
+    <el-input  placeholder="请输入账号" v-model="account" ></el-input>
+    <el-input placeholder="请输入密码" v-model="password" show-password></el-input>
+    <el-button v-on:click="login">登录</el-button>
+  </el-container>
+
+</template>
+<script>
+export default {
+  name: 'Login',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      account :'',
+      password: ''
+    }
+  },
+  methods:{
+    login(){
+      console.log('账号密码是:'+this.account +','+this.password)
+    }
+  }
+}
+</script>
